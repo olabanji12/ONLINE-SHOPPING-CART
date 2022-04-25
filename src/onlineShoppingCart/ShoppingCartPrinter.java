1fdeff7f63caf4d93a82e323ecd6393da526b195
olabanji12 <oao190005@utdallas.edu>
2022-04-24 22:19:58 -0500

    makes minor changes to file

--- src/onlineShoppingCart/ShoppingCartPrinter.java
+++ src/onlineShoppingCart/ShoppingCartPrinter.java
@@ -21,7 +21,7 @@ public class ShoppingCartPrinter {
         System.out.println("Enter the Item quantity: ");
         itemQuantity = scanner.nextInt();
 
-        ItemToPurchase item1 = new ItemToPurchase(itemName, itemPrice, itemQuantity);
+        //ItemToPurchase item1 = new ItemToPurchase(itemName, itemPrice, itemQuantity);
 
         scanner.nextLine();
         System.out.println("Item 2");
@@ -32,15 +32,15 @@ public class ShoppingCartPrinter {
         System.out.println("Enter the Item quantity: ");
         itemQuantity2 = scanner.nextInt();
 
-        ItemToPurchase item2 = new ItemToPurchase(itemName2, itemPrice2, itemQuantity2);
+        //ItemToPurchase item2 = new ItemToPurchase(itemName2, itemPrice2, itemQuantity2);
 
-        int item1Cost = item1.getPrice() * item1.getQuantity();
+        /*int item1Cost = item1.getPrice() * item1.getQuantity();
         int item2Cost = item2.getPrice() * item2.getQuantity();
         int totalCost = item1Cost + item2Cost;
         System.out.println("TOTAL COST");
         System.out.println(item1.getName() + " " + item1.getQuantity() + " @ $" + item1.getPrice() + " = " + item1Cost);
         System.out.println(item2.getName() + " " + item2.getQuantity() + " @ $" + item2.getPrice() + " = " + item2Cost);
-        System.out.println("Total: $" + totalCost);
+        System.out.println("Total: $" + totalCost);*/
 
 
 
