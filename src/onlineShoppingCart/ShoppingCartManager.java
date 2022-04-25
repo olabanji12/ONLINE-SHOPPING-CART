7ad80db476e1057ddd675fc969412eda7b0869de
olabanji12 <oao190005@utdallas.edu>
2022-04-24 22:25:22 -0500

    modifies execteMenu method

--- src/onlineShoppingCart/ShoppingCartManager.java
+++ src/onlineShoppingCart/ShoppingCartManager.java
@@ -1,36 +1,42 @@
 package onlineShoppingCart;
 
-import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class ShoppingCartManager {
     public static void printMenu(){
         System.out.println("MENU");
         System.out.println("a - Add item to cart");
+
         System.out.println("d - Remove item from cart");
+
         System.out.println("c - Change item quantity");
+
         System.out.println("i - Output items' descriptions");
+
         System.out.println("o - Output shopping cart");
+
         System.out.println("q - Quit");
+
     }
 
     public static void executeMenu(char choice, ShoppingCart newCart, Scanner scan) {
         if(choice == 'o'){
-            System.out.println("OUTPUT SHOPPING CART");
             newCart.printTotal();
         }
         else if(choice == 'i'){
-            System.out.println("OUTPUT SHOPPING CART");
             newCart.printDescriptions();
         }
         else if(choice == 'a'){
             System.out.println("ADD ITEM TO CART");
+            scan.nextLine();
             System.out.println("Enter the item name:");
             String name = scan.nextLine();
             System.out.println("Enter the item description:");
             String description = scan.nextLine();
+
             System.out.println("Enter the item price:");
             int price = scan.nextInt();
+            scan.nextLine();
             System.out.println("Enter the item quantity:");
             int quantity = scan.nextInt();
             ItemToPurchase item = new ItemToPurchase(name, description, price, quantity);
@@ -38,12 +44,14 @@ public class ShoppingCartManager {
         }
         else if(choice == 'd'){
             System.out.println("REMOVE ITEM FROM CART");
+            scan.nextLine();
             System.out.println("Enter name of item to remove:");
             String name = scan.nextLine();
             newCart.removeItem(name);
         }
         else if(choice == 'c'){
             System.out.println("CHANGE ITEM QUANTITY");
+            scan.nextLine();
             System.out.println("Enter the item name:");
             String name = scan.nextLine();
             System.out.println("Enter the new quantity:");
@@ -65,8 +73,10 @@ public class ShoppingCartManager {
         custName = scan.nextLine();
         System.out.println("Enter Today's Date:");
         currDate = scan.nextLine();
+        System.out.println();
         System.out.println("Customer Name: " + custName);
         System.out.println("Today's Date: " + currDate);
+        System.out.println();
         ShoppingCart cart = new ShoppingCart(custName,currDate);
         printMenu();
         System.out.println();
@@ -75,12 +85,15 @@ public class ShoppingCartManager {
 
         while(userChoice != 'q'){
             if(userChoice != 'a' && userChoice != 'd' && userChoice != 'c' && userChoice != 'i' && userChoice != 'o'){
+
                 System.out.println("Choose an option:");
                 userChoice = scan.next().charAt(0);
             }
             if(userChoice == 'a' || userChoice == 'd' || userChoice == 'c' || userChoice == 'i' || userChoice == 'o'){
                 executeMenu(userChoice,cart, scan);
+                System.out.println();
                 printMenu();
+                System.out.println();
                 System.out.println("Choose an option:");
                 userChoice = scan.next().charAt(0);
             }
@@ -90,3 +103,4 @@ public class ShoppingCartManager {
 
     }
 }
+
